16a534e168960de770c06970afd479c91ff06e00
Lucas Barcelos <lucavm87@gmail.com>
2025-03-22 21:40:20 -0300

    start a project

---
+++ src/main/java/com/banco/case_contas/CaseContasApplication.java
@@ -0,0 +1,13 @@
+package com.banco.case_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco/case_contas/CaseContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.case_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
