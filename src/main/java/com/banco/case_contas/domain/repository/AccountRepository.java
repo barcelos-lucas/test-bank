72bdf5f85ab70aad34a1c31f280732e5dfb97cb9
Lucas Barcelos <lucavm87@gmail.com>
2025-03-25 00:06:41 -0300

    feat: adiciona a List no repositório de contas

--- src/main/java/com/banco/case_contas/domain/repository/AccountRepository.java
+++ src/main/java/com/banco/case_contas/domain/repository/AccountRepository.java
@@ -4,9 +4,12 @@ import com.banco.case_contas.domain.model.Account;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 public interface AccountRepository {
     Account save(Account account);
     Optional<Account> findById(UUID id);
     boolean existsById(UUID id);
+    List<Account> findAll();
+
 }
