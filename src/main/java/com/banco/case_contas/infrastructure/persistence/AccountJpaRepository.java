6b72e8fc0cb1fad5a7f08c096653d93f91a25545
Lucas Barcelos <lucavm87@gmail.com>
2025-03-25 02:46:18 -0300

    fix: adiciona o import e o List na interface

--- src/main/java/com/banco/case_contas/infrastructure/persistence/AccountJpaRepository.java
+++ src/main/java/com/banco/case_contas/infrastructure/persistence/AccountJpaRepository.java
@@ -3,7 +3,10 @@ package com.banco.case_contas.infrastructure.persistence;
 import com.banco.case_contas.domain.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AccountJpaRepository extends JpaRepository<Account, UUID> {
+    List<Account> findByOwner(String owner);
 }
+
