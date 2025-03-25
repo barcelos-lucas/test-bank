9899698584ebef08d3cbc23a21636861d5772ef4
Lucas Barcelos <lucavm87@gmail.com>
2025-03-25 00:08:29 -0300

    fix: muda o tratamento no registro com a criação da classe RegisterUserUseCase

--- src/main/java/com/banco/case_contas/presentation/web/AuthController.java
+++ src/main/java/com/banco/case_contas/presentation/web/AuthController.java
@@ -1,42 +1,41 @@
 package com.banco.case_contas.presentation.web;
 
+import com.banco.case_contas.application.usecase.RegisterUserUseCase;
 import com.banco.case_contas.domain.model.User;
-import com.banco.case_contas.domain.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final RegisterUserUseCase registerUserUseCase;
 
-    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public AuthController(RegisterUserUseCase registerUserUseCase) {
+        this.registerUserUseCase = registerUserUseCase;
     }
 
+    //login
     @GetMapping("/login")
     public String login() {
-        return "login"; // resources/templates/login.html
+        return "login";
     }
 
+    //registro (GET)
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("user", new User());
         return "register";
     }
 
+    //formulário de registro (POST)
     @PostMapping("/register")
     public String processRegister(@ModelAttribute User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole("USER");
-        userRepository.save(user);
+        registerUserUseCase.execute(user);
         return "redirect:/login";
     }
 
+    // Página inicial depois do login
     @GetMapping("/home")
     public String home() {
         return "home";
